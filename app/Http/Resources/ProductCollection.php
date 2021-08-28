2841e8ac455a31b16caeb5db8da8ee25b9704da2
g1ll <gillvelleda@gmail.com>
2021-08-28 00:34:27 -0300

    Modify method toArray to return data from collecion model.

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -14,6 +14,7 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request)
+        return ['data'=>$this->collection];
     }
 }
