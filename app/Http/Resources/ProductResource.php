7e8ec780ed0058e2c6101874d9379b3483b0be39
g1ll <gillvelleda@gmail.com>
2021-08-27 22:36:12 -0300

    Return all data from resource propertie|model.

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,10 +16,11 @@ class ProductResource extends JsonResource
     {
 //        return parent::toArray($request);
 
-         return  [
-             'name'=>$this->name,
-             'description'=>$this->description,
-             'price'=>$this->price
-         ];
+//         return  [
+//             'name'=>$this->name,
+//             'description'=>$this->description,
+//             'price'=>$this->price
+//         ];
+        return $this->resource->toArray();
     }
 }
