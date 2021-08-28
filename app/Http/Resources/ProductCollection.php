45d274580ae97e97cf8fa1027f5d681ae4959106
g1ll <gillvelleda@gmail.com>
2021-08-28 01:01:30 -0300

    Add extra data with the number of products, total price, and off-price evaluated.

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -15,6 +15,22 @@ class ProductCollection extends ResourceCollection
     public function toArray($request)
     {
 //        return parent::toArray($request)
-        return ['data'=>$this->collection];
+        return [
+            'data'=>$this->collection,
+            'numProducts'=>sizeof($this->collection)//extra data!
+        ];
     }
+
+    public function with($request)
+    {
+        $totalPrice = 0;
+        foreach ($this->collection as $product)
+            $totalPrice+=$product->price;
+
+        return [
+            'totalPrice'=>$totalPrice,
+            'offPrice'=>($totalPrice>100)?$totalPrice *.9:null // Ten percent of off-price;
+        ];
+    }
+
 }
