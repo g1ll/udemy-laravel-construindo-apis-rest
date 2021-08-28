299c7144f74e0bea36870fcaef41f2dd44eed64a
g1ll <gillvelleda@gmail.com>
2021-08-27 22:29:39 -0300

    Create code to return resource data from model product.

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,12 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+
+         return  [
+             'name'=>$this->name,
+             'description'=>$this->description,
+             'price'=>$this->price
+         ];
     }
 }
