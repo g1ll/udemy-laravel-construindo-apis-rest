b80f20454921f210d8e9179cd2e26434b21a7325
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-01 21:43:45 -0300

    Use ProducRepositoy on ProducController index method.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
+use App\Repository\ProductRepository;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
@@ -28,19 +29,22 @@ class ProductController extends Controller
 
             $products = $this->product;
 
-            if($request->has('conditions')){
-                $conditions = explode(';',$request->get('conditions'));
-                foreach ($conditions as $expression) {
-                    $exp = explode(':',$expression);
-                    $products = $products->where($exp[0], $exp[1], $exp[2]);
-                }
-            }
+//            if($request->has('conditions')){
+//                $conditions = explode(';',$request->get('conditions'));
+//                foreach ($conditions as $expression) {
+//                    $exp = explode(':',$expression);
+//                    $products = $products->where($exp[0], $exp[1], $exp[2]);
+//                }
+//            }
+            $products = (new ProductRepository($products,$request))->selectFilter();
 
             if($request->has('fields')){
                 $fields = $request->get('fields');
 //            $products=DB::table('products')->selectRaw($fields)->get();
                 $products = $products->addSelect(explode(',',$fields));
             }
+
+
 
             return response()->json($products->paginate(10));//does not able to use collection
         }
